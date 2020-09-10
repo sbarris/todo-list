92b3a093ba364913d7a3d96936a43606f5928ddb
sbarris <sbarris.dev@gmail.com>
2020-09-10 16:54:03 +0200

    Updates

---
+++ precache-manifest.5938fda91a19087c5901e2b23b302f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f1618ea4e5e7fcb1c5c671f5ce085e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "03906d2b8138c8ca9d53",
+    "url": "/todo-list/static/css/main.0f71efdc.chunk.css"
+  },
+  {
+    "revision": "437812f57929be2dcbc6",
+    "url": "/todo-list/static/js/2.abed89e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.abed89e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03906d2b8138c8ca9d53",
+    "url": "/todo-list/static/js/main.d365320a.chunk.js"
+  },
+  {
+    "revision": "e6c732f71a6591a727cd",
+    "url": "/todo-list/static/js/runtime-main.69335a18.js"
+  }
+]);
